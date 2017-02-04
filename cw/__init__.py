4178b4a95886b4e3d683b1951bed459c0661c8ce
davidvhill <davehill75@gmail.com>
2017-02-04 09:02:34 -0600

    config moved to __init__.py with env vars. additional make targets for setting up the project

--- cw/__init__.py
+++ cw/__init__.py
@@ -2,32 +2,37 @@ from . import messaging
 from . import spark
 #from messaging.sending import Sending
 
-def message_receiver(sysargs):
-    receiver = messaging.Receiving(sysargs)
+config = {'rabbit-host': os.getenv('lcw-rabbit-host', 'localhost'),
+          'rabbit-port': os.getenv('lcw-rabbit-port', 5672),
+          'rabbit-queue': os.getenv('lcw-rabbit-queue': 'unit.lcmap.changes.worker'),
+          'rabbit-exchange': os.getenv('lcw-rabbit-exchange', 'unit.lcmap.changes.worker'),
+          'rabbit-binding': os.getenv('lcw-rabbit-binding', 'unit.b) 'unit.'',
+          'rabbit-ssl': false,
+          'ubid_band_dict' : {
+              'tm': {'red': 'band3',
+                     'blue': 'band1',
+                     'green': 'band2',
+                     'nirs': 'band4',
+                     'swirs1': 'band5',
+                     'swirs2': 'band7',
+                     'thermals': 'band6',
+                     'qas': 'cfmask'},
+              'oli': {'red': 'band4',
+                      'blue': 'band2',
+                      'green': 'band3',
+                      'nirs': 'band5',
+                      'swirs1': 'band6',
+                      'swirs2': 'band7',
+                      'thermals': 'band10',
+                      'qas': 'cfmask'}}}
+
+def message_receiver(cfg):
+    receiver = messaging.Receiving(cfg)
     receiver.start_consuming()
 
-def launch_task(config, msg_body):
+def launch_task(cfg, msg_body):
     # config is a dictionary
     # msg_body needs to be a url
-    return spark.run(config, msg_body)
+    return spark.run(cfg, msg_body)
     #sender = Sending(config)
     #sender.send(msg_body)
-
-ubid_band_dict = {
-    'tm': {'red': 'band3',
-           'blue': 'band1',
-           'green': 'band2',
-           'nirs': 'band4',
-           'swirs1': 'band5',
-           'swirs2': 'band7',
-           'thermals': 'band6',
-           'qas': 'cfmask'},
-    'oli': {'red': 'band4',
-            'blue': 'band2',
-            'green': 'band3',
-            'nirs': 'band5',
-            'swirs1': 'band6',
-            'swirs2': 'band7',
-            'thermals': 'band10',
-            'qas': 'cfmask'}
-}
--- cw/change_worker.py
+++ cw/change_worker.py
@@ -1,12 +1,10 @@
-from cw import message_receiver
-import json
+#!/usr/bin/env python3
+
+import cw
 import sys
 
 def main():
-    # required initialization values coming from STDIN
-    # initiate listener
-    # from STDIN, looking for rabbitmqhost, rabbitmqport, rabbitmqchangequeue, marathonhost, sparkhome
-    message_receiver(json.loads(sys.argv[1]))
+    cw.message_receiver(cw.config)
 
 if __name__ == "__main__":
     main()
