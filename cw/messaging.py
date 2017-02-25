57f7d6fea1458eb9ffa62db0344cc555cc1d74a9
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-02-25 17:31:25 -0600

    Prefetch only one message

--- cw/messaging.py
+++ cw/messaging.py
@@ -9,8 +9,8 @@ def listen(callback_handler, conn, queue):
     # This needs to be manual ack'ing, research and make sure
     # otherwise we'll get multiple deliveries.
     channel.basic_consume(callback_handler,
-                          queue=queue,
-                          no_ack=False)
+                          queue=queue)
+    channel.basic_qos(prefetch_count=1)
     channel.start_consuming()
 
 def send(message, channel, exchange, routing_key):
