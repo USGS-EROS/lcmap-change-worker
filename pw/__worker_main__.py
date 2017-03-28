abdef1c303837fd08a615c92d2d59d6ea3096e99
clay austin <clay.austin.ctr@usgs.gov>
2017-03-28 13:01:42 -0500

    comment out cassandra test for now, fix http launch

--- pw/__worker_main__.py
+++ pw/__worker_main__.py
@@ -1,6 +1,5 @@
 from pw import logger
 from pw import run_http
-from pw import terminate_http
 from pw import HTTP_PORT
 
 import traceback
@@ -8,14 +7,11 @@ import sys
 
 
 def main():
-    http_process = None
     try:
-        http_process = run_http(HTTP_PORT)
+        run_http(HTTP_PORT)
     except Exception as e:
         logger.error('Worker exception running http: {}'.format(e))
         traceback.print_exc(file=sys.stderr)
-    finally:
-        terminate_http(http_process)
 
 
 if __name__ == "__main__":
