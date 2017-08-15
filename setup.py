c8c4fe76a885598fee3b8219d08cf000b1ec0c62
clay austin <clay.austin.ctr@usgs.gov>
2017-08-15 15:01:10 -0500

    add versioning to lcmap-pyccd-worker

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['pika>=0.10.0',
                       'requests>=2.12.4',
-                      'lcmap-pyccd=={}'.format(__version__),
+                      'lcmap-pyccd=={}'.format(__pyccd_version__),
                       'xarray==0.9.1',
                       'pandas==0.19.2',
                       'numpy==1.12.0',
